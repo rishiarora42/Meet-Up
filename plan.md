434e4eec1733577b7c62f9a1481044459deb6f33
Rishi <rishiarora42@gmail.com>
2022-07-11 13:38:03 +0530

    VI CONNECT  DONE

---
+++ plan.md
@@ -0,0 +1,11 @@
+# PLAN OF ACITON
+
+1. Initialize of Nodejs project - DONE
+2. Initialize our first view - DONE
+3. crete a room id - DONE
+4. Add the ability to add our own video - DONE
+5. Add ability to allow others to stream their video - DONE
+6. Add Styling - DONE
+7. Add ability to create Messages - DONE
+8. Add mute button - DONE
+9. Add video stop button - DONE
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.Server(app);
+
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+const { v4: uuidv4 } = require("uuid");
+
+app.use("/peerjs", peerServer);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+  });
+});
+
+server.listen(9000, () => {
+  console.log("Server started at Port: 9000");
+});
